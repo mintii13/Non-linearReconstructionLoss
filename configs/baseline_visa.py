a9f648b8e0748f9168b9c06a237993062087f8a8
Minh Tri Nguyen server2 <chinjsu130205@gmail.com>
2025-12-13 07:40:16 +0700

    add config baseline visa, seede42

---
+++ configs/baseline_visa.py
@@ -0,0 +1,144 @@
+from argparse import Namespace
+from timm.data.constants import IMAGENET_DEFAULT_MEAN
+from timm.data.constants import IMAGENET_DEFAULT_STD
+import torchvision.transforms.functional as F
+
+from configs.__base__ import *
+
+class cfg(cfg_common, cfg_dataset_default, cfg_model_uniad):
+
+    def __init__(self):
+        cfg_common.__init__(self)
+        cfg_dataset_default.__init__(self)
+        cfg_model_uniad.__init__(self)
+
+        # Stats Config
+        self.stats_config = dict(
+            ci_ratio=90,            
+            activation_type='sigmoid',
+            enabled=False             
+        )
+        self.seed = 42
+        self.size = 256
+        
+        # === Trainer Settings ===
+        self.epoch_full = 600
+        self.warmup_epochs = 0
+        self.test_per_epoch = 50
+        self.test_start_epoch = 50
+        
+        self.batch_train = 8
+        self.batch_test_per = 8
+        
+        self.lr = 1e-4 * self.batch_train / 8 
+        self.weight_decay = 0.0001
+        
+        self.metrics = [
+            'mAUROC_sp_max', 'mAUROC_px',
+            # 'mAP_sp_max', 'mF1_max_sp_max',
+            # 'mAUPRO_px', 
+            # 'mAP_px', 'mF1_max_px',
+        ]
+
+        # ==> Data
+        self.data.type = 'DefaultAD'
+        self.data.root = 'data/visa'
+        self.data.meta = 'meta.json'
+        self.data.cls_names = []
+
+        self.data.train_transforms = [
+            dict(type='Resize', size=(self.size, self.size), interpolation=F.InterpolationMode.BILINEAR),
+            dict(type='CenterCrop', size=(self.size, self.size)),
+            dict(type='ToTensor'),
+            dict(type='Normalize', mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD, inplace=True),
+        ]
+        self.data.test_transforms = self.data.train_transforms
+        self.data.target_transforms = [
+            dict(type='Resize', size=(self.size, self.size), interpolation=F.InterpolationMode.BILINEAR),
+            dict(type='CenterCrop', size=(self.size, self.size)),
+            dict(type='ToTensor'),
+        ]
+
+        # ==> Backbone
+        checkpoint_path = 'model/pretrain/tf_efficientnet_b4_aa-818f208c.pth'
+        self.model_backbone = Namespace()
+        self.model_backbone.name = 'timm_tf_efficientnet_b4'
+        self.model_backbone.kwargs = dict(pretrained=True, checkpoint_path='', strict=False, hf=None, features_only=True, out_indices=[0, 1, 2, 3])
+
+        # ==> Decoder Params
+        inplanes = [24, 32, 56, 160]
+        self.model_decoder = dict(
+            inplanes=inplanes,
+            outplanes=[sum(inplanes)],
+            instrides=[16],
+            feature_size=[self.size // 16, self.size // 16],
+            neighbor_size=[self.size // 32, self.size // 32]
+        )
+
+        # Model Config
+        self.model = Namespace()
+        self.model.name = 'baseline' 
+        self.model.kwargs = dict(
+            pretrained=False, 
+            checkpoint_path='', 
+            strict=True, 
+            model_backbone=self.model_backbone,
+            model_decoder=self.model_decoder, 
+            stats_config=self.stats_config
+        )
+
+        # Evaluator, Optimizer
+        self.evaluator.kwargs = dict(metrics=self.metrics, pooling_ks=[16, 16], max_step_aupro=100)
+        self.optim.lr = self.lr
+        self.optim.kwargs = dict(name='adamw', betas=(0.9, 0.999), eps=1e-8, weight_decay=self.weight_decay, amsgrad=False)
+
+        # Trainer config
+        self.trainer.name = 'UniADTrainer' 
+        self.trainer.logdir_sub = ''
+        self.trainer.resume_dir = ''
+        self.trainer.epoch_full = self.epoch_full
+        
+        self.trainer.scheduler_kwargs = dict(
+            name='step', lr_noise=None, noise_pct=0.67, noise_std=1.0, noise_seed=42, lr_min=self.lr / 1e2,
+            warmup_lr=self.lr / 1e3, warmup_iters=-1, cooldown_iters=0, warmup_epochs=self.warmup_epochs, cooldown_epochs=0, use_iters=True,
+            patience_iters=0, patience_epochs=0, decay_iters=0, decay_epochs=int(self.epoch_full * 0.8), cycle_decay=0.1, decay_rate=0.1)
+            
+        self.trainer.mixup_kwargs = dict(mixup_alpha=0.8, cutmix_alpha=1.0, cutmix_minmax=None, prob=0.0, switch_prob=0.5, mode='batch', correct_lam=True, label_smoothing=0.1)
+        self.trainer.test_start_epoch = self.test_start_epoch
+        self.trainer.test_per_epoch = self.test_per_epoch
+
+        self.trainer.data.batch_size = self.batch_train
+        self.trainer.data.batch_size_per_gpu_test = self.batch_test_per
+
+        self.loss.loss_terms = [
+            dict(type='L2Loss', name='pixel', lam=1.0),
+        ]
+
+        self.logging.log_terms_train = [
+            dict(name='batch_t', fmt=':>5.3f', add_name='avg'),
+            dict(name='data_t', fmt=':>5.3f'),
+            dict(name='optim_t', fmt=':>5.3f'),
+            dict(name='lr', fmt=':>7.6f'),
+            dict(name='pixel', suffixes=[''], fmt=':>5.3f', add_name='avg'),
+        ]
+        self.logging.log_terms_test = [
+            dict(name='batch_t', fmt=':>5.3f', add_name='avg'),
+            dict(name='pixel', suffixes=[''], fmt=':>5.3f', add_name='avg'),
+        ]
+        
+        # === WandB ===
+        self.wandb = Namespace()
+        self.wandb.enabled = True
+        self.wandb.project = "Ader_VisA_Global" 
+        self.wandb.entity = None 
+        self.wandb.name = 'Baseline_noSigmoid90_600_lr0.0001_512_seede42'
+        self.wandb.tags = ["mvtec", "baseline", "replica"]
+        self.wandb.notes = "baseline with sigmoid channel."
+        self.wandb.mode = "online"
+        self.wandb.group = None
+        self.wandb.job_type = "train"
+        self.wandb.resume = "allow"
+        self.wandb.run_id = None
+        self.wandb.dir = None
+        self.wandb.login = True
+        self.wandb.api_key = "0f2ca680372a916c31aab5ede7bbefab410fe503"
