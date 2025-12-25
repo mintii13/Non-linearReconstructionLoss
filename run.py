95b4ee643729479e0b4355e21db3114a81f2dac6
Minh Tri Nguyen server2 <chinjsu130205@gmail.com>
2025-12-25 10:32:21 +0700

    baselineLnorm, benTrue

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ def seed_everything(seed=42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.benchmark = True
 def main():
 	parser = argparse.ArgumentParser()
 	# parser.add_argument('-c', '--cfg_path', default='configs/rd_mvtec_debug.py')
