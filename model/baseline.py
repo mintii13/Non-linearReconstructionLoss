8704935ad726517cea0288b797fce2e02566a7d8
Minh Tri Nguyen <chinjsu130205@gmail.com>
2026-01-03 00:37:06 +0700

    baselineLnorm 45

--- model/baseline.py
+++ model/baseline.py
@@ -428,12 +428,12 @@ class BaselineWrapper(nn.Module):
     def forward(self, imgs):
         feats_backbone = self.net_backbone(imgs)
         feats_merge = self.net_merge(feats_backbone)
-        # # 1. Permute
-        # feats_norm = feats_merge.permute(0, 2, 3, 1) # B, H, W, C
-        # # 2. Norm
-        # feats_norm = self.net_norm(feats_norm)
-        # # 3. Permute back
-        # feats_merge = feats_norm.permute(0, 3, 1, 2) # B, C, H, W
+        # 1. Permute
+        feats_norm = feats_merge.permute(0, 2, 3, 1) # B, H, W, C
+        # 2. Norm
+        feats_norm = self.net_norm(feats_norm)
+        # 3. Permute back
+        feats_merge = feats_norm.permute(0, 3, 1, 2) # B, C, H, W
         feats_norm = feats_merge.detach()
         
         output_dict = self.net_ad(feats_norm)
