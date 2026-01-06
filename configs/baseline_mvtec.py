deb00f3b54f25d4d8499c7a3eb7c60618eced265
Minh Tri Nguyen <chinjsu130205@gmail.com>
2026-01-06 23:05:15 +0700

    baselineLnorm

--- configs/baseline_mvtec.py
+++ configs/baseline_mvtec.py
@@ -128,10 +128,10 @@ class cfg(cfg_common, cfg_dataset_default, cfg_model_uniad):
         
         # === WandB ===
         self.wandb = Namespace()
-        self.wandb.enabled = False
+        self.wandb.enabled = True
         self.wandb.project = "Ader_MVTec_Global" 
         self.wandb.entity = None 
-        self.wandb.name = 'Baseline_Sigmoid90_500_lr0.0001_512_seede42'
+        self.wandb.name = 'BaselineLnorm_Sigmoid90_500_lr0.0001_512_seede42'
         self.wandb.tags = ["mvtec", "baseline", "replica"]
         self.wandb.notes = "baseline with sigmoid channel."
         self.wandb.mode = "online"
