33c8056db34e70d4d23bad87bae00b14d2d88763
Minh Tri Nguyen <chinjsu130205@gmail.com>
2026-02-02 21:57:52 +0700

    FnA, noise

--- model/baseline.py
+++ model/baseline.py
@@ -135,10 +135,6 @@ class Baseline(nn.Module):
         
         feature_tokens = self.input_proj(feature_tokens)
         
-        feature_tokens = feature_tokens.permute(1, 2, 0) # -> (B, C, L)
-        feature_tokens = self.instance_norm(feature_tokens)
-        feature_tokens = feature_tokens.permute(2, 0, 1) # -> (L, B, C)
-        
         pos_embed = self.pos_embed(feature_tokens)
         encoded_tokens = self.encoder(feature_tokens, pos=pos_embed)
         decoded_tokens = self.decoder(encoded_tokens, encoded_tokens, pos=pos_embed)
