dfea50f3edb103591461dff1ae4da4f7a378ac8e
Minh Tri Nguyen server2 <chinjsu130205@gmail.com>
2026-02-09 12:30:05 +0700

    FnA_dualmem60%_InB

--- model/baseline.py
+++ model/baseline.py
@@ -363,8 +363,8 @@ class Baseline(nn.Module):
     
     def forward(self, feature_align):
         # feature_align: B x C X H x W
-        # feature_tokens = self.feature_norm(feature_align)
-        feature_norm = rearrange(feature_align, "b c h w -> (h w) b c")
+        feature_tokens = self.feature_norm(feature_align)
+        feature_norm = rearrange(feature_tokens, "b c h w -> (h w) b c")
         
         if self.training and self.feature_jitter:
             feature_tokens = self.add_jitter(feature_norm, self.feature_jitter.scale, self.feature_jitter.prob)
