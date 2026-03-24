85acf22654a0151d911fe29287617b63c9753a01
Minh Tri Nguyen <chinjsu130205@gmail.com>
2026-03-24 13:34:14 +0700

    fix indent

--- run.py
+++ run.py
@@ -9,12 +9,12 @@ warnings.filterwarnings("ignore")
 
 def seed_everything(seed=42):
 	random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
+	np.random.seed(seed)
+	torch.manual_seed(seed)
+	torch.cuda.manual_seed(seed)
 	torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+	torch.backends.cudnn.deterministic = True
+	torch.backends.cudnn.benchmark = False
 def main():
 	parser = argparse.ArgumentParser()
 	# parser.add_argument('-c', '--cfg_path', default='configs/rd_mvtec_debug.py')
