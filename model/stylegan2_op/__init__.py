23bd13998c165ffd7f109a7008a5cf52f8d7e9d9
Minh Tri Nguyen <chinjsu130205@gmail.com>
2026-03-31 15:17:50 +0700

    clean version for kes

--- model/stylegan2_op/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-    Refer to https://github.com/rosinality/stylegan2-pytorch/blob/master/op/__init__.py
-"""
-from .fused_act import FusedLeakyReLU, fused_leaky_relu
-from .upfirdn2d import upfirdn2d
